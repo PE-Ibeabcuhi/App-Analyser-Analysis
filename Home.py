29330dcbd5118bbb5a5eefce85bebde003426ba9
Peter <user@users-Air-2.fritz.box>
2024-01-20 09:43:42 +0000

    Initial commit

---
+++ Home.py
@@ -0,0 +1,187 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+from utils import validate_google_play_link, get_google_play_reviews, sentiment_score, get_app_store_reviews, validate_app_store_link, app_analyzer_description
+
+
+#-- setting up the main page --
+st.set_page_config(page_title="Appstore sentiment Analysis", page_icon=":bar_chart:", layout="wide")
+
+# Create a placeholder for the welcome message
+welcome_placeholder = st.empty()
+
+# Display the welcome message
+welcome_placeholder.markdown(app_analyzer_description(), unsafe_allow_html=True)
+
+# --Setting the side_bar title
+st.sidebar.title('App Analyser')
+
+#-- Reading the data--
+app_link = st.sidebar.text_input("Enter App link here:")
+df = None
+if app_link:
+    app_store_result = validate_app_store_link(app_link)
+    google_play_result = validate_google_play_link(app_link)
+
+    if app_store_result:
+        try:
+            country_code, app_name, app_id = app_store_result
+            with st.spinner(f"Fetching Appstore reviews for {app_name} please hold..."):
+                df = get_app_store_reviews(country_code, app_name, app_id)
+                name = app_name.title()
+                st.markdown(f"<p style='font-size:35px; text-align:center;'>{name} APP Dashboard</p>", unsafe_allow_html=True)
+        except Exception as e:
+            st.sidebar.error("Please check the link. An error occurred while fetching Appstore reviews.")
+            st.stop()
+    elif google_play_result:
+        try:
+            app_name = google_play_result
+            with st.spinner(f"Fetching Playstore reviews for {app_name} please hold..."):
+                df = get_google_play_reviews(app_name)
+                name = app_name.split('com.')
+                name = name[1].title()
+                st.markdown(f"<p style='font-size:35px; text-align:center;'>{name} APP Dashboard</p>", unsafe_allow_html=True)
+        except Exception as e:
+            # Handle the error and display a custom error message
+            st.sidebar.error("Please check the link. An error occurred while fetching Playstore reviews.")
+            st.stop()
+    else:
+        st.sidebar.warning('Invalid link, please check the link')
+
+    
+
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+if df is not None:
+    # Clear the welcome message placeholder
+    welcome_placeholder.empty()
+    # A filter for Years
+    selected_years = st.sidebar.multiselect("Select the Year:",
+    options=df["Year"].unique(),
+    default=df["Year"].unique())
+
+    # A filter for reviews
+    selected_review = st.sidebar.multiselect("Select Review type:",
+    options=df["Sentiments"].unique(),
+    default=df["Sentiments"].unique())
+
+    # A filter for Ratings
+    selected_rating = st.sidebar.multiselect("Select Rating type:",
+    options=df["Ratings"].unique(),
+    default=df["Ratings"].unique())
+
+    # Setting the Title for the Main Page
+
+
+
+    df_selection = df.query("Year == @selected_years & Sentiments == @selected_review & Ratings == @selected_rating")
+    st.dataframe(df_selection)
+
+    # Check if the dataframe is empty:
+    if df_selection.empty:
+        st.warning("No data available based on the current filter settings!")
+        st.stop() # This will halt the app from further execution.
+    
+
+    # TOP KPI's
+    total_reviews = len(df_selection)
+    negative_reviews = len(df_selection[df_selection['Sentiments']=='Negative'])
+    positive_reviews = len(df_selection[df_selection['Sentiments']=='Positive'])
+    average_ratings = df_selection['Ratings'].mean().round(2)
+    star_rating = "⭐️" * int(round(average_ratings, 0))
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.markdown("#### Total Reviews")
+        st.markdown(f"<p style='font-size:20px; text-align:center;'>{total_reviews}</p>", unsafe_allow_html=True)
+
+    with col2:
+        st.markdown("#### Positive Reviews")
+        st.markdown(f"<p style='font-size:20px; text-align:center;'>{positive_reviews}</p>", unsafe_allow_html=True)
+
+    with col3:
+        st.markdown("#### Negative Reviews")
+        st.markdown(f"<p style='font-size:20px; text-align:center;'>{negative_reviews}</p>", unsafe_allow_html=True)
+
+    with col4:
+        st.markdown("#### Average Ratings")
+        st.markdown(f"<p style='font-size:20px; text-align:center;'>{average_ratings} {star_rating}</p>", unsafe_allow_html=True)
+            
+    st.markdown("""---""")
+
+
+    # Reviews count (doughnut chart)
+    sentiment_counts = df_selection['Sentiments'].value_counts()
+
+    fig_sentiment_counts = px.pie(
+        sentiment_counts,
+        values=sentiment_counts.values,
+        names=sentiment_counts.index,
+        title="Sentiment Distribution (Doughnut Chart)",
+        color_discrete_sequence=px.colors.qualitative.Set3,
+        hole=0.5,  # Set the hole parameter to create a doughnut chart
+    )
+    fig_sentiment_counts.update_layout(
+        xaxis=dict(tickmode="linear"),
+        plot_bgcolor="rgba(0,0,0,0)",
+        yaxis=dict(showgrid=False),  # Corrected from yaxis=(dict(showgrid=False))
+    )
+
+    # Ratings count(bar chart)
+    rating_counts = df_selection['Ratings'].value_counts()
+    fig_ratings_counts = px.bar(
+        rating_counts,
+        x="count",
+        y=rating_counts.index,
+        orientation="h",
+        title=" Ratings Performance",  
+        color_discrete_sequence=["#0083B8"] * len(rating_counts),  
+        template="plotly_white",
+    )
+
+    fig_ratings_counts.update_layout(
+        xaxis_visible=False,
+        plot_bgcolor="rgba(0,0,0,0)",
+        yaxis=dict(showgrid=False),  
+    )
+
+    left_column, right_column = st.columns(2)
+    left_column.plotly_chart(fig_sentiment_counts, use_container_width=True)
+    right_column.plotly_chart(fig_ratings_counts, use_container_width=True)
+
+    st.markdown("""---""")
+
+    st.subheader('Common Words')
+
+    #-- Wordcloud
+    df = df_selection['Reviews'].astype(str)
+    # Generate WordCloud for 'Poem' entries in the 'Music' genre with stopwords removed
+    wordcloud = WordCloud(stopwords=STOPWORDS, max_font_size=30).generate(' '.join(df))
+    fig,ax = plt.subplots()
+    ax.imshow(wordcloud)
+    ax.axis('off')
+    st.pyplot(fig)
+
+    st.markdown("""---""")
+
+    st.subheader('Top Poor Feedbacks')
+    # Getting the top negative reviews
+    top_negative = df_selection[df_selection['Sentiments'] == 'Negative'].sort_values(by='Ratings', ascending=True).head(10)
+    top_negative_reviews_df = top_negative['Reviews'] 
+
+    # Displaying the top negative reviews 
+    st.dataframe(top_negative_reviews_df, use_container_width=True)
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
---
+++ utils.py
@@ -0,0 +1,179 @@
+from google_play_scraper import app, Sort, reviews_all
+from app_store_scraper import AppStore
+import pandas as pd
+import numpy as np
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import re
+
+
+# Initiating the transformer
+tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+# Getting the sentiment from BERT LLM
+@st.cache_resource
+def sentiment_score(review):
+    tokens = tokenizer.encode(review, return_tensors='pt')
+    result = model(tokens)
+    return int(torch.argmax(result.logits))+1
+
+# Getting the reviews from google playstore
+@st.cache_data
+def get_google_play_reviews(app_id):
+    try:
+        # Get reviews using play_scraper
+        g_reviews = reviews_all(
+            app_id,
+            sleep_milliseconds=0,  # defaults to 0
+            lang='en',  # defaults to 'en'
+            country='us',  # defaults to 'us'
+            sort=Sort.NEWEST,  # defaults to Sort.MOST_RELEVANT
+        )
+
+        # Convert the reviews to a DataFrame
+        g_df = pd.DataFrame(np.array(g_reviews), columns=['review'])
+        g_df2 = g_df.join(pd.DataFrame(g_df.pop('review').tolist()))
+
+        # Extract relevant columns and rename them
+        df1 = g_df2[['content', 'score', 'at']]
+        df1 = df1.rename(columns={'content': 'Reviews', 'score': 'Ratings', 'at': 'Date'})
+
+        # Add a 'Source' column
+        df1['Source'] = 'Google Play'
+
+        # Adding a year column
+        df1['Year']= df1['Date'].dt.year
+        
+        # Adding a sentiment column
+        j = df1['Reviews'].apply(lambda x: sentiment_score(x[:512]))
+        df1['Sentiments'] = np.select([j.isin([1,2]), j==3, j.isin([4,5])],['Negative','Neutral','Positive'],
+                                    default ='Unknown')
+
+         #Return DataFrame                           
+        return df1
+    except Exception as e:
+        # Catch exceptions and display a custom error message
+        st.error.sidebar("Please check the link. An error occurred while fetching Google Play reviews.")
+        st.stop()
+        
+
+# Getting the reviews from appstores
+@st.cache_data
+def get_app_store_reviews(countrycode, app_name, app_id):
+    try:
+        # Get reviews using app_store_scraper
+        a_reviews = AppStore(countrycode, app_name, app_id)
+        a_reviews.review()
+        
+        # Convert the reviews to a DataFrame
+        a_df = pd.DataFrame(np.array(a_reviews.reviews), columns=['review'])
+        a_df2 = a_df.join(pd.DataFrame(a_df.pop('review').tolist()))
+
+        # Extract relevant columns and rename them
+        df2 = a_df2[['review', 'rating', 'date']]
+        df2 = df2.rename(columns={'review': 'Reviews', 'rating': 'Ratings', 'date': 'Date'})
+
+        # Add a 'Source' column
+        df2['Source'] = 'App Store'
+
+        # Adding a year column
+        df2['Year']= df2['Date'].dt.year
+        
+        # Adding a sentiment analysis
+        j = df2['Reviews'].apply(lambda x: sentiment_score(x[:512]))
+        df2['Sentiments'] = np.select([j.isin([1,2]), j==3, j.isin([4,5])],['Negative','Neutral','Positive'],
+                                    default ='Unknown')
+        
+        return df2
+    except Exception as e:
+        # Catch the exception and display a custom error message
+        st.error.sidebar("Please check the link. An error occurred while fetching Appstore reviews.")
+        st.stop()
+        
+          # Stop further execution
+
+# Validating the link from google play store
+@st.cache_data
+def validate_google_play_link(link):
+    google_play_pattern = r'^https://play\.google\.com/store/apps/details\?id=[a-zA-Z0-9._]+$'
+    
+    if not re.match(google_play_pattern, link):
+        return False  # Invalid link
+
+    # Split the link using '=' and '&' as delimiters
+    link_parts = link.split('=')[1].split('&')
+
+    # Extract the app name (the first part after '=')
+    
+    app_name = link_parts[0]
+    
+    return app_name
+
+# Validating the link from apple store
+@st.cache_data
+def validate_app_store_link(link):
+    apple_store_pattern = r'^https://apps\.apple\.com/[a-z]{2}/app/.+?/id\d+$'
+    
+    if not re.match(apple_store_pattern, link):
+        return None  # Invalid link
+
+    # Split the link using '/'
+    link_parts = link.split('/')
+
+    # Extract components
+    country_code = link_parts[3]
+    app_name = link_parts[5]
+    app_id = link_parts[-1].split("id")[1]
+
+    return country_code, app_name, app_id
+
+import streamlit as st
+
+def app_analyzer_description():
+    content = """
+    # App Analyser: Unveiling the Sentiments behind Your App Choices
+
+    Welcome to App Analyser, a web app designed to analyze your favorite apps in terms of reviews and ratings on both the Google Play Store and Apple App Store. It utilizes the BERT (Bidirectional Encoder Representations from Transformers) model to classify reviews into various sentiment categories.
+
+    ## What Happens Inside
+
+    The web app utilizes the [google-play-scraper](https://pypi.org/project/google-play-scraper/) and [Appstore-scraper](https://pypi.org/project/app-store-scraper/) to fetch data from the Google Play Store and Apple App Store. It has been optimized to exclude unnecessary columns, retaining only those essential for sentiment analysis. After retrieving the data, the BERT model is employed to classify reviews on a scale of 1 to 5. Ratings of 1 and 2 represent negative reviews, 3 signifies a neutral review, while 4 and 5 indicate positive reviews.
+
+    ### Important Notice: Acknowledging Processing Limitations
+
+    You may encounter a slow loading time during the initial run of the analyzer. This is because the web scrapers fetch reviews in small increments and generate new HTTPS requests after every 200 reviews. Additionally, the BERT model processes each individual review before classification, so more reviews mean longer preprocessing time. We are actively working to optimize this process. In the meantime, it's advisable to analyze specific apps, and if you have patience, you can analyze larger apps as well.
+
+    ## Usage
+
+    To use the analyzer, locate your favorite app on the Play Store or App Store, and copy the links. A typical App Store link looks like this:
+
+    ```
+    https://apps.apple.com/ie/app/1password-password-manager/id1511601750
+    ```
+
+    A typical Play Store link looks like this:
+
+    ```
+    https://play.google.com/store/apps/details?id=com.artemchep.keyguard
+    ```
+
+    For more clarity, watch the video below.
+
+    If you're curious and want to see how the app works, here are app links for both App Store and Play Store:
+
+    - **Appstore:** [1Password Password Manager](https://apps.apple.com/ie/app/1password-password-manager/id1511601750)
+    - **Playstore:** [Keyguard](https://play.google.com/store/apps/details?id=com.artemchep.keyguard)
+
+    It is also important to note that the app analyzes links from the specific store provided, so for apps on both Play Store and App Store, each link on both websites will be analyzed separately.
+
+    To download the retrieved data for personal analysis, hover over the CSV table and click on the download icon.
+
+    ## Disclaimer
+
+    This web app is developed as a personal project for educational purposes only. Please refrain from using it for malicious or illegal activities. Use responsibly.
+    """
+    return content
+
